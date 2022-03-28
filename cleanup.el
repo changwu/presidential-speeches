234f92e33980f6a2559ddc7acd3a866f205743e1
Karl Fogel <kfogel@red-bean.com>
2022-03-28 18:41:08 -0500

    Strip HTML from speeches added in commit addac276a
    Also, add code to automate such stripping in the future.  The diff
    from commit addac276a to here is exactly the result of running that
    code on the speeches added in commit addac276a.

---
+++ cleanup.el
@@ -0,0 +1,32 @@
+;;;; Emacs Lisp to help
+
+(defun strip-common-html-from-speech ()
+  "Clean common HTML from a raw Presidential speech transcript.  
+
+Specifically, do the following in this order:
+
+  - replace \"&nbsp;</p>\" with \"\"
+  - replace remaining \"<p>\" and \"</p>\" with \"\"
+  - replace \"&nbsp;<br\\\\s-*/>\" with \"\\n\"
+  - replace \"<br\\\\s-*/>\" with \"\\n\"
+  - replace \"&nbsp;\" with \" \"
+
+Afterwards, the buffer should have only the text of the speech,
+with no HTML elements."
+  (interactive)
+  (goto-char (point-min))
+  (let ((replacements (list (cons "&nbsp;</?p>"       "")
+                            (cons "</?p>"             "")
+                            ;; Sometimes you get a repeat on the next
+                            ;; line.  Handle that just one instance:
+                            (cons "&nbsp;<br\\s-*/>\n&nbsp;<br\\s-*/>"  "\n")
+                            (cons "<br\\s-*/>\n&nbsp;<br\\s-*/>"  "\n")
+                            (cons "<br\\s-*/>\n<br\\s-*/>"  "\n")
+                            (cons "&nbsp;<br\\s-*/>"  "\n")
+                            (cons "<br\\\s-*/>"       "\n")
+                            (cons "&nbsp;$"           "")
+                            (cons "&nbsp;"            " "))))
+    (dolist (elt replacements)
+      (save-excursion
+        (replace-regexp (car elt) (cdr elt) nil (point-min) (point-max)))
+      )))
